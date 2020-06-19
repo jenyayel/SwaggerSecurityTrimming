a26f71e61263c356d993394664334a72517d3a10
Jenya Y <jenyay@gmail.com>
2020-06-19 23:41:41 +0300

    Added controller example

---
+++ src/V3/ValuesProtectedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SwaggerSecurityTrimming
+{
+    [Route("protected")]
+    [Authorize(policy: "can-update")]
+    public class ValuesProtectedController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "secret1", "secret2" };
+        }
+    }
+}
